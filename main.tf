19b5b5159345b4cf72c6698052f21eeb1c184c56
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-04 23:24:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ data "aws_instance" "myec2details" {
     instance_id = var.instance_id_name
 }
 
-resource "aws_key_pair" "deployer" {
+resource "aws_key_pair" "jenkins_key" {
     key_name = var.key_name_name
     public_key = var.public_key_name
 }
 
 resource "aws_instance" "myec2" {
     ami = "data.aws_instance.myec2details.ami
-    key_name = aws_key_pair.deployer.id
+    key_name = aws_key_pair.jenkins_key.id
 
 instance_type = var.instance_type_name
 iam_instance_profile = var.iam_instance_profile_name
