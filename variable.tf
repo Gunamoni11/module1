86e96e249d041be2273acae7583ef21bcddf28ee
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-04 23:31:26 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -23,6 +23,7 @@ variable "az_name" {
 }
 
 variable "public_key_name" {
+    type = "string"
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDG4YqWq/HGpAtE4HhmajGu2umykwjaBH1gUJ0MbTFBNncbSEETwcob8mdoyZ9Em+C/hOSPDycH2TjiSXOfLazioeYvxcMPFG3EokbWIHRpRjXdJKajgYqbBT6DL+ZoPrGHhQ75E0Qn1Jd2ynj19Pue6BCRbiBtj9SUAyK/Ob4W7Z5HbQ8PYpqcA/O50alZIA0gq05PwrsQUIA04h+h/6UXcEXqrAJGpmvc5v5nM78iHS7voCqvdqnx1PrYVcllFofKCt8VNT1UN24PCLJIst9mdGJSFipWCd9jf2TE9MrN2aVaFJuhDCTPmxmR6aud+tBfKS6ZOuwFQvqhai409ZZL jenkins
 "
 }
