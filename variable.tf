a8b78dc4a5ef66536f362b861bcfcf49d084f3c1
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-04 20:53:43 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,46 @@
+variable "profile_name" {
+    type = string
+    default = "default"
+}
+
+variable "region_name" {
+    type = string
+    default = "us-east-2"
+}
+
+variable "key_name_name" {
+    type = string
+    default = "jenkins_key"
+}
+
+variable "instance_id_name" {
+    type = string
+    default = "i-060c990e69a4ed8d9"
+}
+
+variable "instance_type_name" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "az_name" {
+    type = string
+    default = "us-east-2b"
+}
+
+variable "public_key_name" {
+    type = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDG4YqWq/HGpAtE4HhmajGu2umykwjaBH1gUJ0MbTFBNncbSEETwcob8mdoyZ9Em+C/hOSPDycH2TjiSXOfLazioeYvxcMPFG3EokbWIHRpRjXdJKajgYqbBT6DL+ZoPrGHhQ75E0Qn1Jd2ynj19Pue6BCRbiBtj9SUAyK/Ob4W7Z5HbQ8PYpqcA/O50alZIA0gq05PwrsQUIA04h+h/6UXcEXqrAJGpmvc5v5nM78iHS7voCqvdqnx1PrYVcllFofKCt8VNT1UN24PCLJIst9mdGJSFipWCd9jf2TE9MrN2aVaFJuhDCTPmxmR6aud+tBfKS6ZOuwFQvqhai409ZZL jenkins
+"
+}
+
+variable "iam_instance_profile_name" {
+    type = string
+    default = "ec2_full_acess"
+}
+
+
+variable "name_name" {
+    type = string
+    default = "new_world"
+}
